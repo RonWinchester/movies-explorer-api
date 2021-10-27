b89f888b2f37e36908ef8a6358b8dfe63a89c135
Ron Winchester <RonWinchester@yandex.ru>
2021-10-27 21:45:11 +0300

    fix: secure:true

--- controllers/users.js
+++ controllers/users.js
@@ -23,7 +23,7 @@ module.exports.login = (req, res, next) => {
           expires: new Date(Date.now() + 60 * 24 * 3600000),
           httpOnly: true,
           sameSite: 'None',
-          secure: false,
+          secure: true,
         })
         .status(200)
         .send({ token });
